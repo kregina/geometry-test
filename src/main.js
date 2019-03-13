b79b08842ca4b9cc490306f983fc781a35f2999c
Kelly Silva <kelly@kellyregina.com.br>
2019-03-12 22:47:28 -0300

    add mouvemove effect

--- src/main.js
+++ src/main.js
@@ -11,12 +11,34 @@ const areaParalelogramDiv = document.getElementById('area-paralelogram');
 const areaCircleDiv = document.getElementById('area-circle');
 const canvasInfo = document.getElementById('canvas-info');
 const resetButton = document.getElementById('reset-button');
+const circleMove = document.getElementById('mousemove');
+const circleMoveCoordinates = document.getElementById('coordinates-mousemove');
+const circleMoveCoordinateX = document.getElementById('x');
+const circleMoveCoordinateY = document.getElementById('y');
 
 resetButton.onclick = _ => {
   interactions = [];
   canvas.innerHTML = '';
   canvasInfo.style.setProperty('--display-canvas-info', 'none');
 }
+
+document.addEventListener('mousemove', (event) => {
+  const [x, y] = Svg.relativePositionFrom(circleMove, event);
+
+  circleMove.setAttributeNS(null, 'cx', x);
+  circleMove.setAttributeNS(null, 'cy', y);
+
+  circleMoveCoordinates.setAttributeNS(null, 'x', x);
+  circleMoveCoordinates.setAttributeNS(null, 'y', y);
+
+  circleMoveCoordinateX.setAttributeNS(null, 'x', x - 10);
+  circleMoveCoordinateX.setAttributeNS(null, 'y', y - 40);
+  circleMoveCoordinateX.innerHTML = `x: ${x.toFixed(2)}`;
+
+  circleMoveCoordinateY.setAttributeNS(null, 'x', x - 10);
+  circleMoveCoordinateY.setAttributeNS(null, 'y', y - 20);
+  circleMoveCoordinateY.innerHTML = `y: ${y.toFixed(2)}`;
+});
 
 function clickHandler(event) {
   const position = Svg.relativePositionFrom(canvas, event);
